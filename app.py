dc651d3e477ded09b03b8ef9aa56de3beb158b39
ramdas <ramdas@netzary.com>
2015-06-26 04:43:40 +0530

    avatar bugs

--- app.py
+++ app.py
@@ -369,10 +369,8 @@ def file_form():
 	try:
 	    name =get_available_name(path1)
 	    upload.save(name)
-	    
-	except IOError:
-	    return "Unable to Save the File"
-	
+	except IOError as e:    
+	    return "I/O error({0}): {1}".format(e.errno, e.strerror)
 	F= File.create(filename= form.filename.data, path=name)
 	F.save()
 	if F.is_image():
